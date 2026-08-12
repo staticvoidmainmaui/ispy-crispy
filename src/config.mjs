c45a4c192b1b93b0d83f78fe24565b9dcbd9df03
staticvoidmainmaui <62073381+staticvoidmainmaui@users.noreply.github.com>
2026-08-12 15:44:31 -0400

    config

---
+++ src/config.mjs
@@ -0,0 +1,7 @@
+// config.mjs
+export const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
+export const supabaseUrl = process.env.SUPABASE_URL;
+export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+
+export const DEV_USER = "00000000-0000-0000-0000-000000000001";
